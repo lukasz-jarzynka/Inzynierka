b1203d01d67393cdb5cbf0c24166923d967b44f9
Lukasz Jarzynka <l.jarzynka@eduksiazka.pl>
2023-10-19 23:26:28 +0200

    Empty Project

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -2,8 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Note;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,7 +2,6 @@
 
 namespace App\DataFixtures;
 
-use App\Factory\NoteFactory;
 use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
